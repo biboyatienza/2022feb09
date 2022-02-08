770be57dbb333f8413a12ba6f3d1426662c984a5
ca Is BACK <caisback@outlook.com>
2022-02-09 07:58:42 +0800

    Applied express basic

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,16 @@
-const createdBy = '-- 2022.Feb.09 EJS'
-console.log(`Game! ${createdBy}`)
+import express from 'express'
+
+const createdBy = '-- 2022.Feb.09 EJS x'
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/check', (req, res, next) => {
+  res.send(`All OK! ${createdBy}`)
+})
+
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`Game! ${createdBy}`)
+})
