097a5a54c4003fc50a35a208414220d2e90fb945
ca Is BACK <caisback@outlook.com>
2022-02-09 08:49:48 +0800

    Applied ts and error handling

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,32 @@
-import express from 'express'
+import express, {Request, Response, NextFunction, Express} from 'express'
+import morgan from 'morgan'
+import creatHttpError from 'http-errors'
 
-const createdBy = '-- 2022.Feb.09 EJS x'
-const app = express()
+const createdBy = '-- 2022.Feb.09 EJS x TS'
+const app: Express = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(morgan('dev'))
 
-app.get('/check', (req, res, next) => {
+app.get('/check', (req: Request, res: Response, next: NextFunction) => {
   res.send(`All OK! ${createdBy}`)
 })
 
-const PORT = 3000
+// Error Handling
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(new creatHttpError.NotFound())
+})
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status = err.status || 500
+  res.send({
+    status: res.status,
+    message: err.message
+  })
+})
+
+const PORT: Number = Number(process.env.PORT||3000)
 app.listen(PORT, () => {
   console.log(`Game! ${createdBy}`)
 })
